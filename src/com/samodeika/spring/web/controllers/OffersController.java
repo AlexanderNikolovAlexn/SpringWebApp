74a5836fd4d8fadbf70fd12435035d5dc83c902e
Alexander Nikolov <alexander.nikolov.alexn@gmail.com>
2015-04-09 17:59:04 +0300

    playing with Model passing to Views

--- src/com/samodeika/spring/web/controllers/OffersController.java
+++ src/com/samodeika/spring/web/controllers/OffersController.java
@@ -1,13 +1,30 @@
 package com.samodeika.spring.web.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class OffersController {
 	
+/*	@RequestMapping("/")
+	public ModelAndView showHome(){
+		
+		ModelAndView mv = new ModelAndView("home");
+		Map<String, Object> model = mv.getModel();
+		model.put("name", "Alex");		
+		
+		return mv;
+	}*/
+	
 	@RequestMapping("/")
-	public String showHome(){
+	public String showHome(Model model){
+		
+		model.addAttribute("name", "Pesho");
+		
 		return "home";
 	}
 }
